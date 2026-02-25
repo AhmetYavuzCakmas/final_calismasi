74dc31cb86e66397c5a78fb8cb5db88e81d32a93
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-02-25 12:22:24 +0300

    Add files via upload

---
+++ youtube3/src/alexLee3.java
@@ -0,0 +1,29 @@
+
+public class alexLee3 {
+
+	public static void main(String[] args) {
+		Shirt s = new Shirt("blue",'m');
+		
+		System.out.println(s.color);
+		System.out.println(s.size);
+		
+		
+		
+		
+		
+		
+		/*
+		s.putOn();
+		
+		s.setColor("blue");
+		s.setSize('m');
+		
+		System.out.println(s.color);
+		System.out.println(s.size);
+	*/
+	
+	
+	
+	}
+
+}
